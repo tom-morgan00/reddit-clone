412ebee5db25fa4635751c06eb323ba97b3d0f4b
Tom Morgan <thomas.morgan1903@gmail.com>
2022-05-08 13:20:02 +0100

    added community dropdown and modal

--- src/components/Modal/CreateCommunity/CreateCommunityModal.tsx
+++ src/components/Modal/CreateCommunity/CreateCommunityModal.tsx
@@ -6,11 +6,19 @@ import {
   ModalCloseButton,
   ModalBody,
   Flex,
+  Text,
+  Button,
+  ModalFooter,
+  Box,
+  Input,
+  Stack,
+  Checkbox,
+  Icon,
+  Divider,
 } from '@chakra-ui/react';
-import React from 'react';
-import AuthInputs from '../Auth/AuthInputs';
-import OAuthButtons from '../Auth/OAuthButtons';
-import ResetPassword from '../Auth/ResetPassword';
+import React, { useRef, useState } from 'react';
+import { BsFillPersonFill, BsFillEyeFill } from 'react-icons/bs';
+import { HiLockClosed } from 'react-icons/hi';
 
 type CreateCommunityModalProps = {
   open: boolean;
@@ -21,22 +29,139 @@ const CreateCommunityModal: React.FC<CreateCommunityModalProps> = ({
   open,
   handleClose,
 }) => {
+  const MAX_CHARS = 21;
+  const [communityName, setCommunityName] = useState('');
+  const [charsRemaining, setCharsRemaining] = useState(MAX_CHARS);
+  const [communityType, setCommunityType] = useState('public');
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value.length <= MAX_CHARS) {
+      setCommunityName(e.target.value);
+      setCharsRemaining(MAX_CHARS - e.target.value.length);
+    }
+  };
+  const handleCommunityTypeChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setCommunityType(e.target.name);
+  };
+
   return (
     <>
-      <Modal isOpen={open} onClose={handleClose}>
+      <Modal isOpen={open} onClose={handleClose} size="lg">
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader textAlign="center">Title</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody
-            pb={6}
+          <ModalHeader
             display="flex"
-            flexDir="column"
-            alignItems="center"
-            justifyContent="center"
+            flexDirection="column"
+            fontSize={15}
+            padding={3}
           >
-            This is the body
-          </ModalBody>
+            Create Community
+          </ModalHeader>
+          <Divider />
+          <Box as="form" px={3}>
+            <ModalCloseButton />
+            <ModalBody padding="10px 0" display="flex" flexDir="column">
+              <Text fontWeight={600} fontSize={15}>
+                Name
+              </Text>
+              <Text fontSize={11} color="gray.500">
+                Community names including capitalisation cannot be changed
+              </Text>
+              <Text
+                position="relative"
+                top="28px"
+                left={2}
+                width="20px"
+                color="gray.400"
+              >
+                /r
+              </Text>
+              <Input
+                autoFocus
+                position="relative"
+                name="community"
+                value={communityName}
+                size="sm"
+                pl="24px"
+                onChange={handleChange}
+              />
+              <Text
+                fontSize="9pt"
+                color={charsRemaining === 0 ? 'red' : 'gray.500'}
+              >
+                {charsRemaining} Characters remaining
+              </Text>
+              <Box my={4}>
+                <Text fontWeight={600} fontSize={15}>
+                  Community Type
+                </Text>
+                <Stack spacing={2}>
+                  <Checkbox
+                    name="public"
+                    isChecked={communityType === 'public'}
+                    onChange={handleCommunityTypeChange}
+                  >
+                    <Flex align="center">
+                      <Text fontSize="10pt" mr={2}>
+                        Public
+                      </Text>
+                      <Icon as={BsFillPersonFill} color="gray.500" mr={2} />
+                      <Text fontSize="8pt" color="gray.500">
+                        Anyone can view, post, and comment to this community
+                      </Text>
+                    </Flex>
+                  </Checkbox>
+                  <Checkbox
+                    name="restricted"
+                    isChecked={communityType === 'restricted'}
+                    onChange={handleCommunityTypeChange}
+                  >
+                    <Flex align="center">
+                      <Text fontSize="10pt" mr={2}>
+                        Restricted
+                      </Text>
+                      <Icon as={BsFillEyeFill} color="gray.500" mr={2} />
+
+                      <Text fontSize="8pt" color="gray.500">
+                        Anyone can view this community, but only approved users
+                        can post
+                      </Text>
+                    </Flex>
+                  </Checkbox>
+                  <Checkbox
+                    name="private"
+                    isChecked={communityType === 'private'}
+                    onChange={handleCommunityTypeChange}
+                  >
+                    <Flex align="center">
+                      <Text fontSize="10pt" mr={2}>
+                        Private
+                      </Text>
+                      <Icon as={HiLockClosed} color="gray.500" mr={2} />
+                      <Text fontSize="8pt" color="gray.500">
+                        Only approved user can view and submit to this community
+                      </Text>
+                    </Flex>
+                  </Checkbox>
+                </Stack>
+              </Box>
+            </ModalBody>
+          </Box>
+          <ModalFooter bg="gray.100" borderRadius="0 0 10px 10px">
+            <Button
+              variant="outline"
+              height="30px"
+              mr={2}
+              onClick={handleClose}
+            >
+              Close
+            </Button>
+            <Button height="30px" onClick={() => {}}>
+              Create Community
+            </Button>
+          </ModalFooter>
         </ModalContent>
       </Modal>
     </>
